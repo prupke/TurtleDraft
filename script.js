afd73d33976c02a07d477c4682f89657100fdacf
Paul Rupke <paul.rupke@gmail.com>
2017-06-10 12:24:47 -0400

    Script updates
    Small fixes to make it work in IE

--- script.js
+++ script.js
@@ -3,20 +3,20 @@
 
 			var modalActive=false;	
 
-			function detectmobile() { 
+			// function detectmobile() { 
 
-			    if( navigator.userAgent.match(/Android/i)
-			    || navigator.userAgent.match(/webOS/i)
-			    || navigator.userAgent.match(/iPhone/i)
-			    || navigator.userAgent.match(/iPad/i)
-			    || navigator.userAgent.match(/iPod/i)
-			    || navigator.userAgent.match(/BlackBerry/i)
-			    || navigator.userAgent.match(/Windows Phone/i)
-			    ){
+			//     if( navigator.userAgent.match(/Android/i)
+			//     || navigator.userAgent.match(/webOS/i)
+			//     || navigator.userAgent.match(/iPhone/i)
+			//     || navigator.userAgent.match(/iPad/i)
+			//     || navigator.userAgent.match(/iPod/i)
+			//     || navigator.userAgent.match(/BlackBerry/i)
+			//     || navigator.userAgent.match(/Windows Phone/i)
+			//     ){
 
-			        $('main').addClass('mobile');
-			    }
-			}
+			//         $('main').addClass('mobile');
+			//     }
+			// }
 
 			$('.turtle').addClass('turtle-move');
 			$('.draft').addClass('draft-move');
@@ -52,19 +52,30 @@
 		    setTimeout(function() {   
 		    	if(modalActive===false) {
 		     		$('.demo').addClass('view-demo');
+		    		// $('.demo-button-wrapper').addClass('display-none');
+
 		     	}
 		    }, 1850);
 
 		    $('button.nav__buttons').on('click', (function() {
 		    	var modalActive=true;
+		    	$('.demo-button-wrapper').addClass('display-none');
 		    	$('.modal').removeClass('modal-appear');
-		    	$('button.demo').removeClass('view-demo');
+		    	// $('button.demo').removeClass('view-demo');
+
 		    	$(this).next().addClass('modal-appear');
 		    }));
 
-		    $('.demo-next').on('click', (function() {
+		    $('.demo-next').on('click', (function() {    
 		    	$('.demos').addClass('display-none');
-		    	$(this).next().closest('.demos').removeClass('display-none');
+		    	$(this).closest('.demos').next().removeClass('display-none')
+		    	// $(this).next().closest('.demos').removeClass('display-none');
+		    }));
+
+		    $('.demo-prev').on('click', (function() {
+		    	$('.demos').addClass('display-none');
+		    	$(this).closest('.demos').prev().removeClass('display-none')
+		    	// $(this).next().closest('.demos').removeClass('display-none');
 		    }));
 
 		    $('.forgot-password').on('click', (function() {
@@ -74,21 +85,24 @@
 		    }));
 
 		    $('.sign-up-button').on('click', (function() {
+		    	$('.demos').addClass('display-none');
 		    	$('.sign-up-modal').addClass('modal-appear');
 		    	var modalActive=true;
 
 		    }));
 		  
 		    $('button.demo').on('click', (function() {
-		    	$('button.demo').removeClass('view-demo');
-		    	// $('#demo').prop('checked', true); 
 		    	$('aside.demo-start').addClass('modal-appear');
 		    	// $('aside.demo-start').removeClass('display-none');
 		    	$('.demos').addClass('display-none');
 		    	$('.demo-page1').removeClass('display-none');
+		    	$('.demo-button-wrapper').addClass('display-none');
+
 		    }));
 
 		    $('.close-window').on('click', (function() {
+		    	$('.demo-button-wrapper').removeClass('display-none');
+
 		    	$('button.demo').addClass('view-demo');
 		    	var modalActive=false;
 		    }));
@@ -96,6 +110,7 @@
 		    $('.demo-button').on('click', (function() {
 		    	$('.demos').addClass('display-none');
 		    	$('.demo-page1').removeClass('display-none');
+
 		    }));
 
 		});
